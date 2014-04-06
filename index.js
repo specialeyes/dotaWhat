e8d3b3e37b9e0f7966b76ae904a89ef9da4a0e0e
jmhwang7 <jmhwang7@gmail.com>
2014-04-06 13:01:13 -0400

    added index.js file

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Jennifer on 4/4/14.
+ */
+var interaction = {
+
+  init: function() {
+
+  },
+  vizTitles: {
+    titles: ["Parallel Coordinates", "Second Viz", "Third Viz"],
+    subtitles: ["So cool", "what", "idk"]
+  }
+
+};
+$(document).ready(interaction.init);
